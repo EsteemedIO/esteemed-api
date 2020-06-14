4e1da0d625c3fc376a3ba82fd1c68b779d5439d0
Jack Chakerian <outofobxthinking33@gmail.com>
2020-06-13 20:14:18 -0400

    Add slash profile lookup commands

---
+++ src/slashCommands/latest_profiles.js
@@ -0,0 +1,88 @@
+const qs = require('query-string')
+const axios = require('axios')
+const profiles = require('./../util/userProfiles')
+
+exports.handler = async event => {
+  const payload = qs.parse(event.body)
+
+  delayResponse(payload)
+
+  return { statusCode: 200, body: "Loading 10 most current profiles..." }
+}
+
+const delayResponse = async payload => {
+  try {
+    const currentUser = await profiles.loadUsers()
+      .then(users => users.find(user => user.id == payload.user_id))
+
+    var msg = {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Nothing to display"
+          }
+        }
+      ]
+    }
+
+    if (currentUser.is_admin || currentUser.is_owner) {
+      const allProfiles = await profiles.allProfiles()
+      const latestProfilesKeys = Object.keys(allProfiles).slice(0, 10)
+
+      msg.blocks = await Promise.all(latestProfilesKeys.map(async (item) => {
+        try {
+          return profiles.getUser(item)
+            .then(requestedUser => {
+              if (requestedUser.ok) {
+                let text = profiles.format(requestedUser.user.profile)
+
+                if (allProfiles[item].hasOwnProperty('drupal_profile')) {
+                  // text += "\n" + "<" + allProfiles[item].drupal_profile + "|" + allProfiles[item].drupal_bio + ">"
+                }
+
+                if (allProfiles[item].hasOwnProperty('wp_profile')) {
+                  // text += "\n" + "<" + allProfiles[item].wp_profile + "|" + allProfiles[item].wp_bio + ">"
+                }
+
+                return {
+                  "type": "section",
+                  "text": {
+                    "type": "mrkdwn",
+                    "text": text
+                  },
+                  "accessory": {
+                    "type": "image",
+                    "image_url": requestedUser.user.profile.image_48,
+                    "alt_text": requestedUser.user.profile.real_name
+                  }
+                }
+
+                //acc.push()
+              }
+            })
+        } catch (err) { throw err }
+      }))
+      .then(blocks => blocks.flatMap((v, i, a) => a.length - 1 !== i ? [v, { "type": "divider" }] : v))
+    }
+    else {
+      msg.blocks = {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Only admins or owners can use this command"
+        }
+      }
+    }
+
+    //console.log(msg.blocks)
+    axios.post(process.env.SLACK_SLASH_COMMAND_HOOK, msg, { headers: { 'Content-Type': 'application/json' } })
+
+    return
+  } catch (e) {
+    console.log(e)
+
+    return { statusCode: 400, body: JSON.stringify(e) }
+  }
+}
---
+++ src/slashCommands/profile.js
@@ -0,0 +1,86 @@
+const qs = require('query-string')
+const axios = require('axios')
+const profiles = require('./../util/userProfiles')
+
+exports.handler = async event => {
+  const payload = qs.parse(event.body)
+
+  delayResponse(payload)
+
+  return { statusCode: 200, body: "Loading user info..." }
+}
+
+const delayResponse = async payload => {
+  try {
+    const currentUser = await profiles.loadUsers()
+      .then(users => users.find(user => user.id == payload.user_id))
+
+    var msg = {
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Wrong Username"
+          }
+        }
+      ]
+    }
+
+    if (currentUser.is_admin || currentUser.is_owner) {
+      const usersPromise = profiles.loadUsers()
+      const profilesPromise = profiles.allProfiles()
+      const users = await usersPromise
+      const allProfiles = await profilesPromise
+
+      const requestedUser = users.find(user => user.name == payload.text.replace('@', '')) || false
+
+      if (requestedUser) {
+        let text = profiles.format(requestedUser.profile)
+
+        if (allProfiles[requestedUser.id].hasOwnProperty('drupal_profile') ) {
+          // text += "\n" + "<" + allProfiles[requestedUser.id].drupal_profile + "|" + allProfiles[requestedUser.id].drupal_bio + ">"
+        }
+
+        if (allProfiles[requestedUser.id].hasOwnProperty('wp_profile') ) {
+          // text += "\n" + "<" + allProfiles[requestedUser.id].wp_profile + "|" + allProfiles[requestedUser.id].wp_bio + ">"
+        }
+
+        msg = {
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": text
+              },
+              "accessory": {
+                "type": "image",
+                "image_url": requestedUser.profile.image_48,
+                "alt_text": requestedUser.profile.real_name
+              }
+            }
+          ]
+        }
+      }
+    } else {
+      msg.blocks = [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "Only admin or owner can use this command"
+          }
+        }
+      ]
+    }
+
+    axios.post(process.env.SLACK_SLASH_COMMAND_HOOK, msg, { headers: { 'Content-Type': 'application/json' } })
+
+    return
+  } catch (e) {
+    console.log(e)
+
+    return { statusCode: 400, body: JSON.stringify(e) }
+  }
+}
