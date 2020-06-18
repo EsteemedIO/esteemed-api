f3766b4189b0a3a2fe900e198db8c50444cc389e
Jack Chakerian <outofobxthinking33@gmail.com>
2020-06-18 14:23:45 -0400

    Refactor profile lookup slash commands

--- src/slashCommands/latest_profiles.js
+++ src/slashCommands/latest_profiles.js
@@ -5,84 +5,88 @@ const profiles = require('./../util/userProfiles')
 exports.handler = async event => {
   const payload = qs.parse(event.body)
 
-  delayResponse(payload)
-
-  return { statusCode: 200, body: "Loading 10 most current profiles..." }
-}
-
-const delayResponse = async payload => {
   try {
-    const currentUser = await profiles.loadUsers()
-      .then(users => users.find(user => user.id == payload.user_id))
+    Promise.all([ profiles.loadUsers(), profiles.allProfiles() ])
+      .then(([users, allProfiles]) => {
+        const currentUser = users.find(user => user.id == payload.user_id)
 
-    var msg = {
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "Nothing to display"
-          }
+        if (!(currentUser.is_admin || currentUser.is_owner)) {
+          return [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Only admins or owners can use this command"
+              }
+            }
+          ]
         }
-      ]
-    }
 
-    if (currentUser.is_admin || currentUser.is_owner) {
-      const allProfiles = await profiles.allProfiles()
-      const latestProfilesKeys = Object.keys(allProfiles).slice(0, 10)
+        if (users.length == 0) {
+          return [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "No profiles available."
+              }
+            }
+          ]
+        }
 
-      msg.blocks = await Promise.all(latestProfilesKeys.map(async (item) => {
-        try {
-          return profiles.getUser(item)
-            .then(requestedUser => {
-              if (requestedUser.ok) {
-                let text = profiles.format(requestedUser.user.profile)
+        const allProfilesArray = Object.keys(allProfiles).reduce((acc, key) => {
+          acc.push({...allProfiles[key], ...{id: key}})
+          return acc
+        }, [])
 
-                if (allProfiles[item].hasOwnProperty('drupal_profile')) {
-                  // text += "\n" + "<" + allProfiles[item].drupal_profile + "|" + allProfiles[item].drupal_bio + ">"
-                }
+        const latestProfilesKeys = allProfilesArray
+          .sort((a, b) => { return Number(b.join_date.split("-").join("")) - Number(a.join_date.split("-").join("")) })
+          .map(value => value.id)
+          .slice(0, 10)
 
-                if (allProfiles[item].hasOwnProperty('wp_profile')) {
-                  // text += "\n" + "<" + allProfiles[item].wp_profile + "|" + allProfiles[item].wp_bio + ">"
-                }
+        return Promise.all(latestProfilesKeys.map(async item => {
+          try {
+            return profiles.getUser(item)
+              .then(requestedUser => {
+                if (requestedUser.ok) {
+                  let text = profiles.format(requestedUser.user.profile)
 
-                return {
-                  "type": "section",
-                  "text": {
-                    "type": "mrkdwn",
-                    "text": text
-                  },
-                  "accessory": {
-                    "type": "image",
-                    "image_url": requestedUser.user.profile.image_48,
-                    "alt_text": requestedUser.user.profile.real_name
+                  if (allProfiles[item].hasOwnProperty('drupal_profile')) {
+                    // text += "\n" + "<" + allProfiles[item].drupal_profile + "|" + allProfiles[item].drupal_bio + ">"
+                  }
+
+                  if (allProfiles[item].hasOwnProperty('wp_profile')) {
+                    // text += "\n" + "<" + allProfiles[item].wp_profile + "|" + allProfiles[item].wp_bio + ">"
+                  }
+
+                  return {
+                    "type": "section",
+                    "text": {
+                      "type": "mrkdwn",
+                      "text": text
+                    },
+                    "accessory": {
+                      "type": "image",
+                      "image_url": requestedUser.user.profile.image_48,
+                      "alt_text": requestedUser.user.profile.real_name
+                    }
                   }
                 }
-
-                //acc.push()
-              }
-            })
-        } catch (err) { throw err }
-      }))
-      .then(blocks => blocks.flatMap((v, i, a) => a.length - 1 !== i ? [v, { "type": "divider" }] : v))
-    }
-    else {
-      msg.blocks = {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": "Only admins or owners can use this command"
-        }
-      }
-    }
-
-    //console.log(msg.blocks)
-    axios.post(process.env.SLACK_SLASH_COMMAND_HOOK, msg, { headers: { 'Content-Type': 'application/json' } })
-
-    return
+              })
+            } catch (err) { throw err }
+          }))
+          .then(blocks => blocks.flatMap((v, i, a) => a.length - 1 !== i ? [v, { "type": "divider" }] : v))
+        })
+        .then(blocks => axios.post('https://slack.com/api/chat.postMessage', null, {
+            headers: { 'Content-Type': 'application/json' },
+            params: { channel: payload.channel_id, token: process.env.SLACK_TOKEN, parse: 'full', blocks: JSON.stringify(blocks) }
+        }))
+        .catch(e => console.log(e))
   } catch (e) {
     console.log(e)
 
     return { statusCode: 400, body: JSON.stringify(e) }
   }
+
+  return { statusCode: 200, body: "Loading 10 most recent profiles..." }
 }
--- src/slashCommands/profile.js
+++ src/slashCommands/profile.js
@@ -5,49 +5,37 @@ const profiles = require('./../util/userProfiles')
 exports.handler = async event => {
   const payload = qs.parse(event.body)
 
-  delayResponse(payload)
-
-  return { statusCode: 200, body: "Loading user info..." }
-}
-
-const delayResponse = async payload => {
   try {
-    const currentUser = await profiles.loadUsers()
-      .then(users => users.find(user => user.id == payload.user_id))
+    Promise.all([ profiles.loadUsers(), profiles.allProfiles() ])
+      .then(([users, allProfiles]) => {
+        const currentUser = users.find(user => user.id == payload.user_id)
 
-    var msg = {
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "Wrong Username"
+        if (!(currentUser.is_admin || currentUser.is_owner)) {
+          return [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Only admin or owner can use this command"
+              }
+            }
+          ]
+        }
+
+        const requestedUser = users.find(user => user.name == payload.text.replace('@', '')) || false
+
+        if (requestedUser) {
+          let text = profiles.format(requestedUser.profile)
+
+          if (allProfiles[requestedUser.id].hasOwnProperty('drupal_profile') ) {
+            // text += "\n" + "<" + allProfiles[requestedUser.id].drupal_profile + "|" + allProfiles[requestedUser.id].drupal_bio + ">"
           }
-        }
-      ]
-    }
 
-    if (currentUser.is_admin || currentUser.is_owner) {
-      const usersPromise = profiles.loadUsers()
-      const profilesPromise = profiles.allProfiles()
-      const users = await usersPromise
-      const allProfiles = await profilesPromise
+          if (allProfiles[requestedUser.id].hasOwnProperty('wp_profile') ) {
+            // text += "\n" + "<" + allProfiles[requestedUser.id].wp_profile + "|" + allProfiles[requestedUser.id].wp_bio + ">"
+          }
 
-      const requestedUser = users.find(user => user.name == payload.text.replace('@', '')) || false
-
-      if (requestedUser) {
-        let text = profiles.format(requestedUser.profile)
-
-        if (allProfiles[requestedUser.id].hasOwnProperty('drupal_profile') ) {
-          // text += "\n" + "<" + allProfiles[requestedUser.id].drupal_profile + "|" + allProfiles[requestedUser.id].drupal_bio + ">"
-        }
-
-        if (allProfiles[requestedUser.id].hasOwnProperty('wp_profile') ) {
-          // text += "\n" + "<" + allProfiles[requestedUser.id].wp_profile + "|" + allProfiles[requestedUser.id].wp_bio + ">"
-        }
-
-        msg = {
-          "blocks": [
+          return [
             {
               "type": "section",
               "text": {
@@ -62,25 +50,28 @@ const delayResponse = async payload => {
             }
           ]
         }
-      }
-    } else {
-      msg.blocks = [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "Only admin or owner can use this command"
-          }
+        else {
+          return [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Wrong Username"
+              }
+            }
+          ]
         }
-      ]
-    }
-
-    axios.post(process.env.SLACK_SLASH_COMMAND_HOOK, msg, { headers: { 'Content-Type': 'application/json' } })
-
-    return
+      })
+      .then(blocks => axios.post('https://slack.com/api/chat.postMessage', null, {
+          headers: { 'Content-Type': 'application/json' },
+          params: { channel: payload.channel_id, token: process.env.SLACK_TOKEN, parse: 'full', blocks: JSON.stringify(blocks) }
+        }))
+      .catch(e => console.log(e))
   } catch (e) {
     console.log(e)
 
     return { statusCode: 400, body: JSON.stringify(e) }
   }
+
+  return { statusCode: 200, body: "Loading user info..." }
 }
