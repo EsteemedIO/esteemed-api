e7d024152c165c6c07dcfe8c2c66949aa5945f42
Albert Volkman <albert.volkman@gmail.com>
2020-07-15 11:54:05 -0400

    Add stream log

---
+++ src/streams.js
@@ -0,0 +1,8 @@
+exports.record = (event, context, callback) => {
+  event.Records.forEach((record) => {
+    console.log(record.eventID);
+    console.log(record.eventName);
+    console.log('DynamoDB Record: %j', record.dynamodb);
+  });
+  callback(null, `Successfully processed ${event.Records.length} records.`);
+};
