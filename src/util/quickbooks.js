1f32ee85249c23292dd45e1f91c420d87f56d6dd
Albert Volkman <albert.volkman@gmail.com>
2021-05-28 11:12:23 -0400

    Only use refresh token QB API call

--- src/util/quickbooks.js
+++ src/util/quickbooks.js
@@ -12,33 +12,21 @@ export async function getToken() {
     }
   }
 
-  // Prepare access token request (this should only happen once).
-  let body = `grant_type=authorization_code&code=${process.env.QBO_AUTH_CODE}&redirect_uri=https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl`
-
   // Check for existing token.
   const token = await fs.readFile(tokenPath, 'utf-8')
-    .then(token => JSON.parse(token))
+    .then(token => token.trim())
     .catch(() => false)
 
-  if (token && token.expire_date > Math.floor(Date.now() / 1000)) {
-    // Check if refresh token is expired.
-    return token.access_token
-  }
-  else if (token) {
-    // Prepare refresh token request.
-    body = `grant_type=refresh_token&refresh_token=${token.refresh_token}`
-  }
+  // Prepare refresh token request.
+  const body = `grant_type=refresh_token&refresh_token=${token}`
 
   return axios.post(`https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer`, body, params)
     .then(({ data }) => {
       // Store token with calculated expiration timestamp.
-      fs.writeFile(tokenPath, JSON.stringify({
-        ...data,
-        expire_date: Math.floor(Date.now() / 1000) + data.x_refresh_token_expires_in
-      }))
+      fs.writeFile(tokenPath, data.refresh_token)
 
       return data.access_token
     })
-    .catch(data => console.log(data))
+    .catch(({ response }) => console.log(response.data))
 
 }
