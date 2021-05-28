882d6990c4b753cd8019d7b5d3811fa59b296031
Albert Volkman <albert.volkman@gmail.com>
2021-05-28 10:10:27 -0400

    Return all placements

--- src/models/placements.js
+++ src/models/placements.js
@@ -4,13 +4,28 @@ const { stringify } = qs
 
 export default {
   get: async email => {
+    let allRecords = []
     const params = {
       fields: 'candidate(email),dateBegin,dateEnd,payRate,clientBillRate,jobOrder(clientCorporation)',
-      query: `status:Approved`
+      query: `status:Approved`,
+      count: 200
     }
 
-    return bhFetch('search/Placement?' + stringify(params))
-      .then(res => res.data.data.map(({ _score, ...array }) => array))
+    // Iterate queries to account for 500 record limit (200 record limit when
+    // querying skills).
+    async function doQuery(start) {
+      if (start) params.start = start
+
+      return await bhFetch('search/Placement?' + stringify(params))
+        .then(res => {
+          allRecords = allRecords.concat(res.data.data)
+          return res.data.data.length >= params.count ? doQuery(allRecords.length) : allRecords
+        })
+        .then(data => data.map(({ _score, ...data }) => data))
+        .catch(e => console.error(e))
+    }
+
+    return doQuery()
       .then(res => res.filter(placement => placement.candidate.email == email))
       .catch(err => console.log(err))
   },
