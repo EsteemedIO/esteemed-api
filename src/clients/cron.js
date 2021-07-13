56cb514ea2cceb39a3a324db3f03f43c50b5aece
Albert Volkman <albert.volkman@gmail.com>
2021-07-13 15:59:16 -0400

    Remove scheduled invoice runs

--- src/clients/cron.js
+++ src/clients/cron.js
@@ -1,10 +1,7 @@
 import { default as cron } from 'node-cron'
 
-import { getHours, reduceEmails } from '../util/clockify.js'
 import { jobs as dbJobs, locationFormat } from '../models/jobs.js'
 import { leads } from '../models/leads.js'
-import placements from '../models/placements.js'
-import { createInvoice, convertClockifyToQBInvoice } from '../models/invoice.js'
 
 export default function() {
   if (process.env.NODE_ENV !== 'production') {
@@ -37,17 +34,5 @@ export default function() {
         .then(async newLeads => await Promise.all(newLeads.map(lead => leads.add(lead))))
         .then(res => res.map(lead => console.log(`Reference ${lead.data.data.firstName} ${lead.data.data.lastName} added as a lead.`)))
     }
-  })
-
-  // Every 2 weeks (on the 1st and 15th).
-  cron.schedule('30 1 1,15 * *', async () => {
-    // Create invoices.
-    const projectHours = await getHours('projectName')
-      .then(placements => placements.flat())
-    const emails = reduceEmails(projectHours)
-    const projectPlacements = await Promise.all(emails.map(async email => await placements.get(email)))
-    const invoices = await convertClockifyToQBInvoice(projectHours, projectPlacements)
-
-    invoices.map(companyHours => createInvoice(companyHours))
   })
 }
