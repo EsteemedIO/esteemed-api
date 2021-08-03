88920b80c6dbbd8b52d9b02b5fbb32b8303dffe6
Albert Volkman <albert.volkman@gmail.com>
2021-08-03 14:04:07 -0400

    Add Quickbooks connection

---
+++ src/util/quickbooks.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import btoa from 'btoa'
+import { promises as fs } from 'fs'
+
+const basicAuth = btoa(`${process.env.QBO_CLIENT_ID}:${process.env.QBO_CLIENT_SECRET}`)
+const tokenPath = './quickbooks-token'
+
+export async function getToken() {
+  const params = {
+    headers: {
+      'Authorization': `Basic ${basicAuth}`
+    }
+  }
+
+  // Check for existing token.
+  const token = await fs.readFile(tokenPath, 'utf-8')
+    .then(token => token.trim())
+    .catch(() => false)
+
+  // Prepare refresh token request.
+  const body = `grant_type=refresh_token&refresh_token=${token}`
+
+  return axios.post(`https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer`, body, params)
+    .then(({ data }) => {
+      // Store token with calculated expiration timestamp.
+      fs.writeFile(tokenPath, data.refresh_token)
+
+      return data.access_token
+    })
+    .catch(({ response }) => console.log(response.data))
+
+}
