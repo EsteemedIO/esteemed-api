6625904e78baa3860ca5bd3ba5433a894c12dc74
Albert Volkman <albert.volkman@gmail.com>
2021-08-03 14:02:34 -0400

    Add model: placements

---
+++ src/models/placements.js
@@ -0,0 +1,42 @@
+import { fetch as bhFetch } from '../util/bullhorn.js'
+import qs from 'qs'
+const { stringify } = qs
+
+export default {
+  get: async email => {
+    return getAll()
+      .then(res => res.filter(placement => placement.candidate.email == email))
+  },
+  getAll: async () => {
+    let allRecords = []
+    const params = {
+      fields: 'candidate(email),dateBegin,dateEnd,payRate,clientBillRate,jobOrder(clientCorporation)',
+      query: `status:(Approved OR Completed OR Terminated)`,
+      count: 200
+    }
+
+    // Iterate queries to account for 500 record limit (200 record limit when
+    // querying skills).
+    async function doQuery(start) {
+      if (start) params.start = start
+
+      return await bhFetch('search/Placement?' + stringify(params))
+        .then(res => {
+          allRecords = allRecords.concat(res.data.data)
+          return res.data.data.length >= params.count ? doQuery(allRecords.length) : allRecords
+        })
+        .then(data => data.map(({ _score, ...data }) => data))
+        .catch(e => console.error(e))
+    }
+
+    return doQuery()
+      .then(placements => placements.map(placement => ({
+          ...placement,
+          candidate: {
+            ...placement.candidate,
+            email: placement.candidate.email.toLowerCase()
+          }
+      })))
+      .catch(err => console.log(err))
+  },
+}
