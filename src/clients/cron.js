758c83982f89d38d064d5426da464af4ea77b853
Albert Volkman <albert.volkman@gmail.com>
2022-05-03 16:17:33 -0400

    Remove unnecessary check

--- src/clients/cron.js
+++ src/clients/cron.js
@@ -12,30 +12,26 @@ export default function() {
 
   // Update jobs cache.
   cron.schedule('*/30 * * * *', async () => {
-    if (process.env.NODE_ENV == 'production') {
-      const key =  '__express__/jobs'
-      cache.flush('/jobs')
+    const key =  '__express__/jobs'
+    cache.flush('/jobs')
 
-      dbJobs.getAll()
-        .then(jobs => jobs.map(job => ({
-            ...job,
-            address: locationFormat(job.address)
-        })))
-        .then(jobs => {
-          cache.setKey(key, JSON.stringify(jobs))
-          cache.save()
-          console.log('Jobs cache refreshed')
-        })
-    }
+    dbJobs.getAll()
+      .then(jobs => jobs.map(job => ({
+          ...job,
+          address: locationFormat(job.address)
+      })))
+      .then(jobs => {
+        cache.setKey(key, JSON.stringify(jobs))
+        cache.save()
+        console.log('Jobs cache refreshed')
+      })
   })
 
   // Add new references as leads.
   cron.schedule('*/30 * * * *', async () => {
-    if (process.env.NODE_ENV == 'production') {
-      leads.getNew()
-        .then(async newLeads => await Promise.all(newLeads.map(lead => leads.add(lead))))
-        .then(res => res.map(lead => console.log(`Reference ${lead.data.data.firstName} ${lead.data.data.lastName} added as a lead.`)))
-    }
+    leads.getNew()
+      .then(async newLeads => await Promise.all(newLeads.map(lead => leads.add(lead))))
+      .then(res => res.map(lead => console.log(`Reference ${lead.data.data.firstName} ${lead.data.data.lastName} added as a lead.`)))
   })
 
   cron.schedule('0 * * * *', async () => {
