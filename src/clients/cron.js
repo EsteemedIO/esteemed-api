8c14a66bb5af8b72e712e6a6efcf1e330fa37edc
Albert Volkman <albert.volkman@gmail.com>
2022-05-10 14:45:48 -0400

    Add daily Slack reminder about job priorities

--- src/clients/cron.js
+++ src/clients/cron.js
@@ -1,3 +1,4 @@
+import { app } from './slack.js'
 import axios from 'axios'
 import { Cron as cron } from 'croner'
 import { default as cache } from '../util/cache.js'
@@ -9,6 +10,33 @@ export default function() {
   if (process.env.NODE_ENV !== 'production') {
     return
   }
+
+  cron('0 8 * * *', async () => {
+    const jobs = await dbJobs.getPriorityJobs(1)
+    const dollarUS = Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    })
+    const blocks = jobs
+      .map(job => (
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `Title: *<https://cls42.bullhornstaffing.com/BullhornSTAFFING/OpenWindow.cfm?Entity=JobOrder&id=${job.id}|${job.title}>* \nCompany: *${job.company}* \nType: *${job.type}* \nPay: *${dollarUS.format(job.pay)}/hour*`
+          }
+        }
+      ))
+      .flatMap(job => [job, {
+          type: 'divider'
+        }]).slice(0, -1)
+
+    app.client.chat.postMessage({
+      token: process.env.SLACK_TOKEN_BOT,
+      channel: 'C03EVBP5ZCK',
+      blocks: blocks
+    })
+  })
 
   // Update jobs cache.
   cron('*/30 * * * *', async () => {
