d2a41428c7c902b1d22a26f7e2838c4e92f98af2
Albert Volkman <albert.volkman@gmail.com>
2022-06-01 14:18:04 -0400

    Fix options block

--- src/blocks/invoice.js
+++ src/blocks/invoice.js
@@ -2,7 +2,10 @@ import { getPayPeriods } from '../models/invoice.js'
 
 export function invoicePeriods() {
   return getPayPeriods().map(payPeriod => ({
-      "text": `${payPeriod.startDate.toLocaleDateString()} - ${payPeriod.endDate.toLocaleDateString()}`,
+      "text": {
+        "type": "plain_text",
+        "text": `${payPeriod.startDate.toLocaleDateString()} - ${payPeriod.endDate.toLocaleDateString()}`,
+      },
       "value": `option-${payPeriod.id}`
   }));
 }
